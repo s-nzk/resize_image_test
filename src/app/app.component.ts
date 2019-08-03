51a36afa9c40c9296d70a96113fd068cea153c1b
nzk <nozaky2017@gmail.com>
2019-08-03 23:09:06 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, ViewChild, ElementRef } from "@angular/core";
+import Pica from "pica";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'anglarnzk';
+  pica = Pica();
+
+  @ViewChild("mycanvas", { static: false }) canvas: ElementRef;
+  onChange(files: File[]): any {
+    const file: File = files[0];
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = (data: ProgressEvent) => {
+      const img = new Image();
+      img.addEventListener(
+        "load",
+        async () => {
+          await this.pica.resize(img, this.canvas.nativeElement);
+          console.log("done");
+          (this.canvas.nativeElement as HTMLCanvasElement).toBlob((blob) => {
+            // serve blob to server
+            console.log(blob);
+          });
+        },
+        false
+      );
+      img.src = (data.target as FileReader).result.toString();
+    };
+  }
 }
